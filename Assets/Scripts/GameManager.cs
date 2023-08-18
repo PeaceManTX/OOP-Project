4d20039b84b82d1d94542140d36618886ad145cf
PeaceManTX <yerksbydesign@gmail.com>
2023-08-18 13:58:14 -0500

    Scene Basics
    created basic scene management.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class UIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartNew()
     {
-        
+        SceneManager.LoadScene(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
